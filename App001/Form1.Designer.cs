9b5a88c4ba4cb34c930807dcfab6cb5202c5860c
svendrami <62160419+svendrami@users.noreply.github.com>
2020-08-22 11:20:07 -0300

    commit

---
+++ App001/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace App001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Tb_num1 = new System.Windows.Forms.TextBox();
+            this.Tb_num2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Lb_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sair";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Número 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Número 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resultado:";
+            // 
+            // Tb_num1
+            // 
+            this.Tb_num1.Location = new System.Drawing.Point(99, 35);
+            this.Tb_num1.Name = "Tb_num1";
+            this.Tb_num1.Size = new System.Drawing.Size(78, 20);
+            this.Tb_num1.TabIndex = 4;
+            // 
+            // Tb_num2
+            // 
+            this.Tb_num2.Location = new System.Drawing.Point(99, 69);
+            this.Tb_num2.Name = "Tb_num2";
+            this.Tb_num2.Size = new System.Drawing.Size(78, 20);
+            this.Tb_num2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(217, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Somar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Lb_result
+            // 
+            this.Lb_result.AutoSize = true;
+            this.Lb_result.Location = new System.Drawing.Point(109, 108);
+            this.Lb_result.Name = "Lb_result";
+            this.Lb_result.Size = new System.Drawing.Size(13, 13);
+            this.Lb_result.TabIndex = 7;
+            this.Lb_result.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 220);
+            this.Controls.Add(this.Lb_result);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Tb_num2);
+            this.Controls.Add(this.Tb_num1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Tb_num1;
+        private System.Windows.Forms.TextBox Tb_num2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Lb_result;
+    }
+}
+
---
+++ App001/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resultado;
+            num1 = Convert.ToInt32(Tb_num1.Text);
+            num2 = Convert.ToInt32(Tb_num2.Text);
+            resultado = num1 + num2;
+            Lb_result.Text = Convert.ToString(resultado);
+        }
+    }
+}
